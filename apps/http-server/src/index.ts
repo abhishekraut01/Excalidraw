86a2b27c3c97704c0d47a5f9a348af17f5e36ed7
Abhishek <Abhishekraut8697@gmail.com>
2025-07-30 03:05:45 +0530

    writing backend

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -2,12 +2,31 @@ import express from "express"
 const app = express()
 const PORT = 4000
 
-app.get('/hello' , (req,res)=>{
+app.use(express.json())
+
+app.get('/signup' , (req,res)=>{
+    const {username , password} = req.body
+
+    if(!username || !password){
+        res.json({
+            success:false,
+            
+        })
+    }
+})
+
+
+app.get('/signin' , (req,res)=>{
     res.json({
         message:"hello"
     })
 })
 
+app.get('/create-room' , (req,res)=>{
+    res.json({
+        message:"hello"
+    })
+})
 
 app.listen(PORT , ()=>{
     console.log( `server is on in port ${PORT}`)
