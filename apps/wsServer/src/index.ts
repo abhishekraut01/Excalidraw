65b117217e0a5a0c9c64fdbc202ae0d6c0311cac
Abhishek <Abhishekraut8697@gmail.com>
2025-07-31 01:20:46 +0530

    adding custom websocket server

--- apps/wsServer/src/index.ts
+++ apps/wsServer/src/index.ts
@@ -1,9 +1,33 @@
 import { WebSocketServer } from 'ws';
+import jwt from 'jsonwebtoken';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+const checkLogin = (token: string): jwt.JwtPayload | false => {
+  if (!token) {
+    return false;
+  }
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string) as jwt.JwtPayload;
+  if (!decoded.userId) {
+    return false;
+  }
+  return decoded;
+};
+
+wss.on('connection', function connection(ws, req) {
   ws.on('error', console.error);
+
+  const token = req.headers.cookie;
+  if (!token) {
+    ws.close();
+    return;
+  }
+
+  const decoded = checkLogin(token);
+  if (!decoded) {
+    ws.close();
+    return;
+  } 
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
